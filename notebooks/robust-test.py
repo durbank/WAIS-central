21de949f116d432859ff9cc7641d44c2366e09c4
Durban G. Keeler <durban.keeler@gmail.com>
2020-06-12 21:06:14 -0600

    Performs bias stats analysis

--- notebooks/robust-test.py
+++ notebooks/robust-test.py
@@ -11,7 +11,6 @@ import numpy as np
 import pandas as pd
 import geopandas as gpd
 from pathlib import Path
-import time
 
 # Set project root directory
 ROOT_DIR = Path(__file__).parent.parent
@@ -85,77 +84,6 @@ gpd_2016 = gpd.GeoDataFrame(
     crs="EPSG:4326")
 
 
-
-
-#%%
-from sklearn.neighbors import BallTree
-
-def get_nearest(
-    src_points, candidates, k_neighbors=1):
-    """Find nearest neighbors for all source points from a set of candidate points"""
-
-    # Create tree from the candidate points
-    tree = BallTree(candidates, leaf_size=15, metric='haversine')
-
-    # Find closest points and distances
-    distances, indices = tree.query(src_points, k=k_neighbors)
-
-    # Transpose to get distances and indices into arrays
-    distances = distances.transpose()
-    indices = indices.transpose()
-
-    # Get closest indices and distances (i.e. array at index 0)
-    # note: for the second closest points, you would take index 1, etc.
-    closest = indices[0]
-    closest_dist = distances[0]
-
-    # Return indices and distances
-    return (closest, closest_dist)
-
-
-def nearest_neighbor(left_gdf, right_gdf, return_dist=False, planet_radius=6371000):
-    """
-    For each point in left_gdf, find closest point in right GeoDataFrame and return them.
-    When return_dist=True, also returns the distance between the nearest points for each entry.
-    The planet radius (in meters) defaults to Earth's radius. 
-
-    NOTICE: Assumes that the input Points are in WGS84 projection (lat/lon).
-    """
-
-    left_geom_col = left_gdf.geometry.name
-    right_geom_col = right_gdf.geometry.name
-
-    # Ensure that index in right gdf is formed of sequential numbers
-    right = right_gdf.copy().reset_index(drop=True)
-
-    # Parse coordinates from points and insert them into a numpy array as RADIANS
-    left_radians = np.array(
-        left_gdf[left_geom_col].apply(
-        lambda geom: (geom.x * np.pi / 180, geom.y * np.pi / 180)).to_list())
-    right_radians = np.array(
-        right[right_geom_col].apply(
-        lambda geom: (geom.x * np.pi / 180, geom.y * np.pi / 180)).to_list())
-
-    # Find the nearest points
-    # -----------------------
-    # closest ==> index in right_gdf that corresponds to the closest point
-    # dist ==> distance between the nearest neighbors (in meters)
-
-    closest, dist = get_nearest(src_points=left_radians, candidates=right_radians)
-
-    # Return points from right GeoDataFrame that are closest to points in left GeoDataFrame
-    closest_points = right.loc[closest]
-
-    # Ensure that the index corresponds the one in left_gdf
-    closest_points = closest_points.reset_index(drop=True)
-
-    # Add distance if requested
-    if return_dist:
-        # Convert to meters from radians
-        closest_points['distance'] = dist * planet_radius
-
-    return closest_points
-
 #%%
 df_dist = nearest_neighbor(
     gpd_2011, gpd_2016, return_dist=True)
@@ -176,24 +104,54 @@ accum_2011 = a2011_ALL[
 std_2011 = std2011_ALL[
     std2011_ALL.columns[idx_2011]].to_numpy()
 accum_2016 = a2016_ALL.iloc[:,dist_2016.idx].to_numpy()
-std_2016 = a2016_ALL.iloc[:,dist_2016.idx].to_numpy()
+std_2016 = std2016_ALL.iloc[:,dist_2016.idx].to_numpy()
 
-# %%
+#%% 
+# Calculate and compare robust linear regression
+trend_2011, lb_2011, ub_2011 = trend_bs(
+    pd.DataFrame(accum_2011, index=a2011_ALL.index), 
+    500)
 
+trend_2016, lb_2016, ub_2016 = trend_bs(
+    pd.DataFrame(accum_2016, index=a2016_ALL.index), 
+    500)
+
+gdf_traces['accum'] = accum_2011.mean(axis=0)
 accum_res = accum_2016 - accum_2011
-gdf_traces['res_mu'] = accum_res.mean(axis=0)
+gdf_traces['res_mu'] = accum_res.mean(axis=0) \
+    / gdf_traces.accum
+gdf_traces['res_trend'] = (trend_2016 - trend_2011) \
+    / gdf_traces.accum
 
+res_perc = accum_res / gdf_traces.accum.to_numpy()
+# %%
+print(
+    f"The mean bias in annual accumulation "
+    f"between 2011 and 2016 flights is "
+    f"{res_perc.mean()*100:.2f}% "
+    f"with a RMSE of {res_perc.std()*100:.2f}% "
+    f"and a RMSE in mean accumulation of {gdf_traces.res_mu.std()*100:.2f}%"
+)
 
+print(
+    f"The RMSE values compare favorably to the mean "
+    f"standard deviations of the annual accumulation "
+    f"estimates of "
+    f"{(std_2011/accum_2011).mean()*100:.2f}% for " f"2011 and "
+    f"{(std_2016/accum_2016).mean()*100:.2f}% for 2016."
+)
 
+print(
+    f"The mean annual trend bias between 2011 and 2016 flights is {gdf_traces.res_trend.mean()*100:.2f}% "
+    f"with a RMSE of {gdf_traces.res_trend.std()*100:.2f}%"
+)
+
+#%%
 import matplotlib.pyplot as plt
 fig, ax = plt.subplots()
 gdf_traces.plot(
     column='res_mu', cmap='coolwarm', ax=ax, 
     legend=True)
-
-
-#%% 
-# Calculate and compare robust linear regression
 
 
 
